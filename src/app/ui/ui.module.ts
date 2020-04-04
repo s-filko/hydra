d1f12be91bf089e8fe33b0be4627428342e9b5b7
filko <s.a.filko@gmail.com>
2020-04-04 18:35:05 +0300

    Fixes

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UsersComponent } from './users/users.component';
 import { UserEditComponent } from './users/user-edit/user-edit.component';
 import { UserListComponent } from './users/user-list/user-list.component';
 import { LayoutComponent } from './layout/layout.component';
@@ -11,7 +10,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-    declarations: [UsersComponent, UserEditComponent, UserListComponent, LayoutComponent, BreadcrumbComponent],
+    declarations: [
+      UserEditComponent,
+      UserListComponent,
+      LayoutComponent,
+      BreadcrumbComponent
+    ],
     exports: [
         LayoutComponent,
     ],
